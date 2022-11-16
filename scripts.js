274fec8b22071344486d32dcf8b6f29b0a59c140
Npvpr <nlmnever2@gmail.com>
2022-11-16 20:10:45 +0630

    day3 js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,21 @@
+// get round and wins
+let roundSpan = document.querySelector('.round')
+let round = 1;
+let playerWonDiv = document.querySelector('.playerWon')
+let comWonDiv = document.querySelector('.comWon')
+let playerWon = 0, comWon = 0;
+
+// get layers
+let comLayer = document.querySelector('.comLayer')
+let playerLayer = document.querySelector('.playerLayer')
+
+// get announcement and player btns
+let announcement = document.querySelector('.announcement')
+let announceBtns = document.querySelectorAll('.announceBtns > button')
+let resetBtn = document.querySelector('.resetBtn')
+let nextBtn = document.querySelector('.nextBtn')
+let playerBtns = document.querySelectorAll('.playerBtns > button')
+
 // make com div's width same as player div's, since player div's width is chaning due to buttons
 let comDiv = document.querySelector('.comDiv')
 let playerDiv = document.querySelector('.playerDiv')
@@ -14,11 +32,16 @@ function animate(img, imager){
 }
 
 // choose function
-let announcement = document.querySelector('.announcement')
-let announceBtns = document.querySelectorAll('.announceBtns > button')
-let playerBtns = document.querySelectorAll('.playerBtns > button')
 let comAnimating, playerAnimating;
 function choose(){
+    // show current round
+    roundSpan.textContent = round;
+    // show wins
+    comWonDiv.textContent = comWon;
+    playerWonDiv.textContent = playerWon;
+    // hide layers
+    comLayer.style.display = "none";
+    playerLayer.style.display = "none";
     // start both animations
     comAnimating = setInterval(() => animate(comImg, "com"), 150);
     playerAnimating = setInterval(() => animate(playerImg, "player"), 150);
@@ -30,18 +53,68 @@ function choose(){
     });
     // when player click one of the player buttons
     playerBtns.forEach(element => {
+        element.style.display = "inline";
         element.addEventListener("click", e => {
             play(e);
         })
     })
 }
-choose();
+
+// Tie
+function tie(){
+    comLayer.style.backgroundColor = "rgba(0, 0, 255, 0.5)";
+    comLayer.style.display = "block";
+    playerLayer.style.backgroundColor = "rgba(0, 0, 255, 0.5)";
+    playerLayer.style.display = "block";
+    announcement.textContent = "Oh! It is a Tie!"
+    roundEnd();
+}
+// Win
+function win(){
+    comLayer.style.backgroundColor = "rgba(255, 0, 0, 0.5)";
+    comLayer.style.display = "block";
+    playerLayer.style.backgroundColor = "rgba(0, 255, 0, 0.5)";
+    playerLayer.style.display = "block";
+    announcement.textContent = "Yayyy! You Won!"
+    playerWon++;
+    roundEnd();
+}
+// Lost
+function lost(){
+    comLayer.style.backgroundColor = "rgba(0, 255, 0, 0.5)";
+    comLayer.style.display = "block";
+    playerLayer.style.backgroundColor = "rgba(255, 0, 0, 0.5)";
+    playerLayer.style.display = "block";
+    announcement.textContent = "Oops! You Lost!"
+    comWon++;
+    roundEnd();
+}
+// round end, wait for rematch or next round
+function roundEnd(){
+    announceBtns.forEach(element => {
+        element.style.display = "block";
+    });
+    comWonDiv.textContent = comWon;
+    playerWonDiv.textContent = playerWon;
+    resetBtn.addEventListener("click", () => {
+        round = 1;
+        comWon = 0;
+        playerWon = 0;
+        choose();
+    })
+    nextBtn.addEventListener("click", () => {
+        round++;
+        choose();
+    })
+}
+
+
 // play function
 function play(e){
     // player's choice
-    let playerChoice = e.target.className.toString().slice(0,-3);
+    let playerChoice = e.target.className.toString().slice(0,1).toUpperCase() + e.target.className.toString().slice(1,-3);
     let comChoice = images[Math.floor(Math.random()*3)].slice(0,-4)
-    console.log(playerChoice, comChoice)
+    console.log(playerChoice == comChoice)
     // hide all buttons
     playerBtns.forEach(element => {
         element.style.display = "none"
@@ -69,16 +142,21 @@ function play(e){
         setTimeout(function(){
             clearInterval(comAnimating);
             comImg.src = "./images/com" + comChoice + ".png";
-        }, 3000)
+            // compare results
+            if(playerChoice == comChoice){
+                tie();
+            }else if((playerChoice == "Rock" && comChoice == "Scissors") 
+                    || (playerChoice == "Paper" && comChoice == "Rock")
+                    || (playerChoice == "Scissors" && comChoice == "Paper")){
+                win();
+            }else{
+                lost();
+            }
+        }, 2000)
     }, 500)
-    // compare results
-    // show red and green colors for win and lose
-    // change announcement
-    // show announcement buttons
-    // depends on the button, change wins and round
-    // call choose function
 }
 
+choose();
 
 // let choices = ["rock", "paper", "scissors"];
 // let player_win = com_win = 0;
@@ -101,7 +179,7 @@ function play(e){
 
 //         // call compare
 //         compare(player_choice, com_choice);
-//     }  
+//     }
 // }
 
 // // Compare
